9516e9ca029ff63b4a61389d9d394d7065d6d565
Allan <allanvictor.almeida@gmail.com>
2020-11-28 05:01:59 -0300

    1. + 2. | Fatoramento dos relações e frequência

--- scripts/script.R
+++ scripts/script.R
@@ -1,29 +1,78 @@
-
 ###### Carregando os pacotes ######
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(ggplot2, readr, dplyr, forcats)
+library(reshape2)
+library(purrr)
 
 #=================================================#
+# 1. | Carregando dados 
+# 2. | Medidas basicas
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+#=================================================#
 
-###### Carregando os dados ######
+####  1. ##### Carregando os dados ######
+
 # Definindo banco de dados
-amostra <- read_csv('data/saeb.csv')
+dados <- read_csv('data/saeb.csv') # 5271 observações 
+
 
 # Selecionando as colunas
-amostra <- select(amostra, LOCALIZACAO, RACA_COR, SEXO, ESC_MAE, NOTA_LP, NOTA_MT, AFAZERES_DOM)
+dados <- select(amostra, LOCALIZACAO, RACA_COR, SEXO, ESC_MAE, NOTA_LP, NOTA_MT, AFAZERES_DOM)
+
+
+# Fatoramento das colunas com string
+dados$LOCALIZACAO <- factor(dados$LOCALIZACAO)
+dados$RACA_COR <- factor(dados$RACA_COR)
+dados$SEXO <- factor(dados$SEXO)
+dados$ESC_MAE <- factor(dados$ESC_MAE)
+dados$AFAZERES_DOM <- factor(dados$AFAZERES_DOM)  
+
+
+# Ordena RACA_COR, AFAZERES_DOM e ESC_MAE
+dados$RACA_COR <- dados$RACA_COR %>% 
+  fct_relevel(c("Não quero declarar",
+                "Amarela",
+                "Branca",
+                "Indígena",
+                "Parda",
+                "Preta")) 
+
+dados$AFAZERES_DOM <- dados$AFAZERES_DOM %>% 
+  fct_relevel(c("Não faço trabalhos domésticos", 
+                "Menos de 1 hora",
+                "Entre 1 e 2 horas",
+                "Mais de 2 horas, até 3 horas",
+                "Mais de 3 horas"))
+  
+dados$ESC_MAE <- dados$ESC_MAE %>% 
+  fct_relevel(c("Não sei", 
+                "Nunca estudou",
+                "Não completou a 4.ª série/5.º ano do Ensino Fundamental",
+                "Completou a 4.ª série/5.º ano, mas não completou a 8.ª série/9.º ano do Ensino Fundamental",
+                "Completou a 8.ª série/9.º ano do Ensino Fundamental, mas não completou o Ensino Médio",
+                "Completou o Ensino Médio, mas não completou a Faculdade",
+                "Completou a Faculdade"))
+
 
 #=================================================#
 
-mae <- unique(amostra$ESC_MAE) # Escolaridade da mãe
+#### 2. ##### Medidas basicas ######
 
-afzr <- unique(amostra$AFAZERES_DOM) # Tempos de afazeres domesticos
+# Total de alunos de cada fator
+freq <- map(select(dados, -c(NOTA_LP,NOTA_MT)), ~select(data.frame(table(.x)), Freq, .x ))
 
-amostra <- amostra %>%
-  mutate(
-    ESC_MAE = factor(ESC_MAE, # ordena a escolaridade da mãe
-      labels = c(mae[4], mae[7], mae[5], mae[6], mae[2], mae[1], mae[3])),
-    AFAZERES_DOM = factor(AFAZERES_DOM, # ordena afazeres domésticos
-      labels = c(afzr[5], afzr[2], afzr[3], afzr[4], afzr[1]))
-)
 
-ggplot(amostra, aes())
+
+
+
+
+#=================================================#
+
+
