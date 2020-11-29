15d92f5b86968a874ccfe0dbdfc9fb56a6d311c5
Allan <allanvictor.almeida@gmail.com>
2020-11-29 02:11:17 -0300

    Descrições dos dados

--- scripts/script.R
+++ scripts/script.R
@@ -1,11 +1,32 @@
 ###### Carregando os pacotes ######
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(ggplot2, readr, dplyr, forcats, reshape, purrr, data.table)
+pacman::p_load(ggplot2, readr, dplyr, forcats, reshape, purrr, data.table, MASS)
 
+
+#================== Relações =====================#
+# 
+# -------------- Descrições ----------------------#
+# 
+# - Quantitativa:
+# Continua: NOTAS_(MT/LP)
+# Discreta: AFAZERES_DOM
+# 
+# - Qualitativa:
+# Nominal: LOCALIZACAO, SEXO, RACA_COR
+# Ordinal: AFAZERES_DOM, ESC_MAE
+# ------------------------------------------------#
+# 
+# -- Teste + Fator --- Interesse -----------------# 
+#
+# RD || RI + Loc --- Notas || Afazeres
+#  RD & RI + Raça_cor --- Notas e Afazeres
+#       RD + Esc_mae --- Notas
+#       RI + sexo --- Afazeres
+# 
 #=================================================#
 # 1. | Carregando dados 
 # 2. | Medidas basicas
-# 
+# 3. | Testes
 # 
 # 
 # 
@@ -39,7 +60,7 @@ dados <- dados %>%
         "Parda",
         "Preta")),
     SEXO = factor(SEXO),
-    ESC_MAE = factor(ESC_MAE) %>% fct_relevel( # reordenando fatores
+    ESC_MAE = factor(ESC_MAE) %>% fct_relevel( # Reordenando fatores
       c("Não sei", 
         "Nunca estudou",
         "Não completou a 4.ª série/5.º ano do Ensino Fundamental",
@@ -48,25 +69,55 @@ dados <- dados %>%
         "Completou o Ensino Médio, mas não completou a Faculdade",
         "Completou a Faculdade")),
     AFAZERES_DOM = factor(AFAZERES_DOM) %>% fct_relevel(
-      c("Não faço trabalhos domésticos", # reordenando fatores
-        "Menos de 1 hora", # 
+      c("Não faço trabalhos domésticos", # Reordenando fatores
+        "Menos de 1 hora", 
         "Entre 1 e 2 horas", 
         "Mais de 2 horas, até 3 horas",
-        "Mais de 3 horas"))
+        "Mais de 3 horas")) %>% fct_collapse("Não faz ou faz menos de 1 hora" = c("Não faço trabalhos domésticos",
+                                                                                  "Menos de 1 hora"))
 )
 
 #=================================================#
 
 #### 2. ##### Medidas basicas ######
 
-# Total de alunos de cada fator
-freq <- map(select(dados, -c(NOTA_LP,NOTA_MT)), ~select(data.frame(table(.x)), Freq, .x ))
+# Total de alunos para cada fator (Frequencia)
+freq <- map(select(dados, -c(NOTA_LP,NOTA_MT)), # Dados
+            ~select(data.frame(table(.x)), Freq, variavel= .x ))  # Função
 
 
+#------------ Resumo das relações ----------------#
 
 
+# (Medias-Resumo) para as relações "LOCALIZACAO","RACA_COR","ESC_MAE" com as NOTAS_(MT/LP)
+{basico_notas <- list()
+  for (fator in c("LOCALIZACAO","RACA_COR","ESC_MAE")) { # Fator
+  basico_notas[[fator]] <- list()
+    for (nota in c("NOTA_MT", "NOTA_LP")){ # Interesse
+      valor <- aggregate(as.formula(paste(nota ,"~" , fator)), dados, summary) # Est. Basicas
+      basico_notas[[fator]][[nota]] <- data.frame(Grupo = valor[,1],  valor[,2]) # data. frame
+    }
+  }
+}
+
+# ?? Proporção ??? para as relações "LOCALIZACAO", "RACA_COR", "SEXO" com AFAZERES_DOM
+
+{basico_afazer <- list()
+  for (fator in c("LOCALIZACAO", "RACA_COR", "SEXO")) {
+    basico_afazer[[fator]] <- round(prop.table(table(select(dados,fator,AFAZERES_DOM)),2),2) # Proporção por coluna (2)
+  }
+}
 
 
 #=================================================#
+
+#### 2. ##### Testes ######
+
+
+
+
+
+
+
 
 
