e6313ffff6279713e6182c9bee72bd8f6d24868f
Allan <allanvictor.almeida@gmail.com>
2020-12-07 23:20:39 -0300

    Grficos box-plot

--- scripts/plots.R
+++ scripts/plots.R
@@ -46,7 +46,16 @@ ggplot(saeb, aes(x = RACA_COR, y = NOTAS, color = RACA_COR)) +
     theme_minimal() +
     labs(x = 'Raça/Cor', y = 'Nota',
     caption = 'Fonte: SAEB 2017') +
-    ggsave('img/raca_cor_notas.png')
+  theme(legend.box = "vertical",
+        legend.position="right") +
+  guides(color = guide_legend(label.position =  "right",
+                              title="Raça/cor",
+                              ncol=1,
+                              reverse = T)) +
+    ggsave('img/raca_cor_notas.pdf',
+           width = 7.6,
+           height = 7,
+           dpi = 500)
 
 # Escolaridade da mãe por notas
 ggplot(saeb, aes(color = ESC_MAE, x = NOTAS))+ 
